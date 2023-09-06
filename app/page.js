87ee07d4372ef3142f0eb13a9c9d9fd40865e0ec
Eason <leoling127@gmail.com>
2023-09-06 21:56:15 +0800

    init todo list

--- app/page.js
+++ app/page.js
@@ -1,94 +1,58 @@
-import Image from 'next/image'
+'use client'
+import { useState } from 'react'
 import styles from './page.module.css'
 
+const inputInit = ''
+
 export default function Home() {
+  const [inputTxt, setInputTxt] = useState(inputInit)
+  const [list, setList] = useState([
+    { id: 1, content: 'abc' },
+    { id: 2, content: 'bbb' },
+  ])
+
+  const handleItemSave = () => {
+    if (inputTxt.trim() === '') return
+    setList((last) => {
+      const { id } = last[last.length - 1] || {}
+      const lastId = id + 1 || 1
+      last.push({ id: lastId, content: inputTxt })
+      return last
+    })
+
+    setInputTxt(inputInit)
+  }
+
+  const handleItemDelete = (index) => {
+    setList((last) => {
+      const tempLast = Array.from(last)
+      const newList = tempLast.splice(index, 1)
+      return tempLast
+    })
+    setInputTxt(inputInit)
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className={styles.container}>
+        <h1>ToDo List</h1>
+        <div className={styles.left}>
+          <input className={styles.input} type="text" value={inputTxt} onChange={(e) => setInputTxt(e.target.value)} />
+          <button className={styles.btn} onClick={handleItemSave}>
+            儲存
+          </button>
         </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <ul className={styles.right}>
+          {list.map(({ id, content }, index) => (
+            <li key={id} className={styles.li}>
+              <p className={styles.content}>{content}</p>
+              <button className={styles.removeBtn} onClick={() => handleItemDelete(index)}>
+                X
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
   )
